875aba5d4cf171297796ec5f2421ab5647ec0384
Tomas Glabazna <tomas@glabazna.cz>
2024-06-23 20:47:44 +0200

    cmt

---
+++ src/components/OldStyle.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>i am an Option API component</div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      first: 'Láďa',
+      last: 'Novák'
+    }
+  },
+  computed: {
+    full () {
+      return this.first + ' ' + this.last
+    }
+  }
+}
+
+
+
+</script>
+
+
---
+++ src/components/TComponent.vue
@@ -0,0 +1,68 @@
+
+<template>
+  <div>{{ label }}</div>
+  <div>{{ counter }}</div>
+  <button @click="increment">plus</button>
+  <div>{{ greeting }}</div>
+  <button @click="setFirstname('Franta')">set to Franta</button>
+  <button @click="setFullName('Jenda Benda')">set to jenda</button>
+</template>
+
+<script setup>
+  import { ref, computed, onMounted, onBeforeMount } from 'vue'
+
+  defineProps({
+    label: {
+      type: String,
+      default: 'i am label'
+    }
+  })
+
+  // code about counter functionality
+  const counter = ref(1)
+  const increment = () => {
+    counter.value += 1
+  }
+  // -----------
+
+
+
+  // code about something else
+  const firstname = ref('Karel')
+  const lastname = ref('Houska')
+
+
+  const fullname = computed({
+      get () {
+        return firstname.value + ' ' + lastname.value
+      },
+      set (str) {
+        const splited = str.split(' ')
+        firstname.value = splited[0]
+        lastname.value = splited[1]
+      }
+    })
+
+  function setFullName (str) {
+    fullname.value = str
+  }
+
+
+  const greeting = computed(() => {
+    return 'hello ' + fullname.value
+  })
+  const setFirstname = (name) => {
+    firstname.value = name
+  }
+
+  onBeforeMount(() => {
+    console.log('beforeMount')
+  })
+
+  onMounted(() => {
+    console.log('mounted')
+  })
+
+
+
+</script>
